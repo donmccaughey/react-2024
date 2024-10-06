e4862c13b82fb8b822252fc9c92adfe0e1470077
Don McCaughey <don@donm.cc>
2024-10-06 15:22:18 -0700

    Fix key name from JSON response.

--- src/BottomNav.js
+++ src/BottomNav.js
@@ -1,13 +1,13 @@
 import {createElement} from "react";
 
-export function BottomNav({ totalPages }) {
+export function BottomNav({ total_pages }) {
     return createElement(
         'nav',
         { key: 'bottom-nav' },
         createElement(
             'p',
             null,
-            'page 1 of ' + totalPages,
+            'page 1 of ' + total_pages,
         ),
     );
 }
