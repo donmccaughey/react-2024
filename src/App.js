5e623698009195fbe33285f1286043505e67c7d0
Don McCaughey <don@donm.cc>
2024-10-06 21:12:17 -0700

    Get the latest news on a five minute timer.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import { Main } from './Main'
 import { Footer } from './Footer'
 
 
+const GET_NEWS_INTERVAL_MS = 5 * 60 * 1000;
+
+
 export function App() {
     const [news, setNews] = useState(emptyNews);
 
     useEffect(() => {
         getNews().then(setNews);
+
+        const intervalId = setInterval(() => {
+            getNews().then(setNews);
+        }, GET_NEWS_INTERVAL_MS);
+
+        return () => clearInterval(intervalId);
     }, []);
 
     return [
